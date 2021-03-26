4185a196461b8f26cd2a20a49ff50d946cfb79df
Nabilaa Noor'Aini <71251110+nabilaa1454@users.noreply.github.com>
2021-03-26 11:53:21 +0700

    Add files via upload

---
+++ Praktikum01/Prak2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>Contoh</head>
+<body>
+	<h1>Heading</h1>
+	<?php
+		echo "<p>Hello world</p>";
+	?>
+</body>
+</html>
---
+++ Praktikum01/Project3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+	<center>
+	<h1>Pemrograman Web</h1>
+	<?php
+		echo "<p>Nama: Nabilaa Noor'Aini</p>";
+		echo "<p>NIM : K3519061</p>";
+	?>
+	</center>
+</body>
+</html>
